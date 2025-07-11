22f61b265ba794f562da1fd94e07c81f32921901
Illia Siutkin <illiasiutkin07@gmail.com>
2025-07-10 22:18:12 -0500

    Create barcode scanner component

---
+++ src/components/Scanner.tsx
@@ -0,0 +1,80 @@
+import {CameraView, CameraType, useCameraPermissions, BarcodeType} from 'expo-camera';
+import {useState} from 'react';
+import {Button, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+
+export const VALID_BARCODE_TYPES: BarcodeType[] = [
+    'ean13',
+    'ean8',
+    'upc_e',
+    'upc_a',
+];
+
+export function Scanner() {
+    const [facing, setFacing] = useState<CameraType>('back');
+    const [permission, requestPermission] = useCameraPermissions();
+
+    if (!permission) {
+        // Camera permissions are still loading.
+        return <View/>;
+    }
+
+    if (!permission.granted) {
+        // Camera permissions are not granted yet.
+        return (
+            <View style={styles.container}>
+                <Text style={styles.message}>We need your permission to show the camera</Text>
+                <Button onPress={requestPermission} title="grant permission"/>
+            </View>
+        );
+    }
+
+    function toggleCameraFacing() {
+        setFacing(current => (current === 'back' ? 'front' : 'back'));
+    }
+
+    return (
+        <View style={styles.container}>
+            <CameraView style={styles.camera} facing={facing} barcodeScannerSettings={{
+                barcodeTypes: VALID_BARCODE_TYPES,
+            }} onBarcodeScanned={(scanned) => {
+                console.log("Scanned", scanned)
+            }}>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+                        <Text style={styles.text}>Flip Camera</Text>
+                    </TouchableOpacity>
+                </View>
+            </CameraView>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    message: {
+        textAlign: 'center',
+        paddingBottom: 10,
+    },
+    camera: {
+        flex: 1,
+    },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: 'transparent',
+        margin: 64,
+    },
+    button: {
+        flex: 1,
+        alignSelf: 'flex-end',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+});
